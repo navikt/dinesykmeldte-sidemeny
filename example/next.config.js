8e38c4fbd66272721e43a78a2b57c44650bc6b8d
Karl J. Overå <k@rl.run>
2022-08-03 15:02:55 +0200

    [pages] fix github pages deploy

--- example/next.config.js
+++ example/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    basePath: process.env.NODE_ENV === 'production' ? '/dinesykmeldte-sidemeny' : undefined,
 };
 
 module.exports = nextConfig;
