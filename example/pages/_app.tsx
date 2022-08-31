6c2f3a0266f900eab02276579c70880230a544da
Karl J. Overå <k@rl.run>
2022-08-31 13:14:01 +0200

    add husky and lint-staged

--- example/pages/_app.tsx
+++ example/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 import '@navikt/dinesykmeldte-sidemeny/dist/style.css';
 
-
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
 import { setBreadcrumbs } from '@navikt/nav-dekoratoren-moduler';
