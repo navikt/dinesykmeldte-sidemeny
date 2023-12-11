09224d5d819d39e4caeb55178e61e703f6c4ca1f
Karl J. Overå <k@rl.run>
2023-12-11 13:17:06 +0100

    fix pages deploy

--- example/next.config.js
+++ example/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     reactStrictMode: true,
     basePath: process.env.NODE_ENV === 'production' ? '/dinesykmeldte-sidemeny' : undefined,
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
